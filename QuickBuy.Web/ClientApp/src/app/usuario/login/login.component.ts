6e166697aa3232fd19da0b63ec2125f88eb20f59
Iago Freitas <IagoFreitas@outlook.com>
2020-12-08 00:55:14 -0300

    inicio da criação dos serviços

--- QuickBuy.Web/ClientApp/src/app/usuario/login/login.component.ts
+++ QuickBuy.Web/ClientApp/src/app/usuario/login/login.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Usuario } from '../../modelo/usuario';
 
 @Component({
@@ -7,18 +8,28 @@ import { Usuario } from '../../modelo/usuario';
   styleUrls: ["./login.component.css"]
 })
 
-export class LoginComponent
+export class LoginComponent implements OnInit
 {
   public image = '../../assets/quickbuy.png';
   public usuario: Usuario;
+  public returnUrl: string;
 
-  constructor()
+  constructor(private router: Router, private activateRouter: ActivatedRoute)
   {
+    
+  }
+
+  ngOnInit(): void
+  {
+    this.returnUrl = this.activateRouter.snapshot.queryParams['returnUrl'];
     this.usuario = new Usuario();
   }
 
   entrar(): void
   {
-    
+    if (this.usuario.email == "iagofreitas05@gmail.com" && this.usuario.senha == "abc") {
+      sessionStorage.setItem("usuario-autenticado", "1");
+      this.router.navigate([this.returnUrl]);
+    }
   }
 }
